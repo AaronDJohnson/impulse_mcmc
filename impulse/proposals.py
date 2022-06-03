6f3cd5d421a6f5f9081b96fb8e90f4fddb3026a3
Aaron <Aaron9034@Hotmail.com>
2022-06-03 08:41:32 -0500

    new proposal scheme

--- impulse/proposals.py
+++ impulse/proposals.py
@@ -6,13 +6,16 @@ from impulse.random_nums import rng
 
 
 class JumpProposals():
-    def __init__(self, ndim, buf_size=10000, groups=None, cov=None, mean=None):
+    def __init__(self, ndim, buf_size=1000, groups=None, cov=None, mean=None):
         """
         ndim (int): number of dimensions in the parameter space
         """
         self.prop_list = []
+        self.prop_weights = []
+        self.prop_probs = []
         self.ndim = ndim
         self._buffer = np.zeros((buf_size, ndim))
+        self._fullbuffer = False  # check if enough samples for DE jumps
 
         # setup sampling groups
         self.groups = groups
@@ -34,35 +37,53 @@ class JumpProposals():
             self.mean = 0
 
     def add_jump(self, jump, weight):
-        self.prop_list.append()
+        self.prop_list.append(jump)
+        self.prop_weights.append(weight)
+        self.prop_probs = self.prop_weights / sum(self.prop_weights)
 
+    def recursive_update(self, sample_num, new_chain):
+        # update buffer
+        if sample_num > len(self._buffer):
+            self._fullbuffer = True
+        self._buffer = new_chain
 
+        # get new sample mean and covariance
+        self.mean, self.cov = update_covariance(sample_num, self.cov, self.mean, self._buffer)
 
-
-class ProposalMix():
-    def __init__(self, ndim, prop_list, cov=None, mean=None):
-        """
-        ndim (int): number of dimensions in the parameter space
-        prop_list (list of tuples): (Proposal, weight)
-        """
-        self.ndim = ndim
-        self.cov = cov
-        self.mean = mean
-        if cov is None:
-            self.cov = np.identity(ndim) * 0.01**2
-        if mean is None:
-            self.mean = 0
-        self.prop_list = list(map(list, zip(*prop_list)))
-
-    def update(self, old_length, new_chain, **kwargs):
-        chain = kwargs.get('full_chain', None)
-        self.mean, self.cov = update_covariance(old_length, self.cov, self.mean, new_chain)
-        for proposal in self.prop_list[0]:
-            proposal.update(cov=self.cov, chain=chain)
+        # new SVD on groups
+        self.U, self.S = svd_groups(self.U, self.S, self.groups, self.cov)
 
     def __call__(self, x):
-        proposal = rng.choice(self.prop_list[0], p=self.prop_list[1])
+        if sum(self.prop_probs) != 1:
+            self.prop_probs /= sum(self.prop_probs)  # normalize the probabilities
+        proposal = rng.choice(self.prop_list, p=self.prop_probs)
         return proposal(x)
+
+
+# class ProposalMix():
+#     def __init__(self, ndim, prop_list, cov=None, mean=None):
+#         """
+#         ndim (int): number of dimensions in the parameter space
+#         prop_list (list of tuples): (Proposal, weight)
+#         """
+#         self.ndim = ndim
+#         self.cov = cov
+#         self.mean = mean
+#         if cov is None:
+#             self.cov = np.identity(ndim) * 0.01**2
+#         if mean is None:
+#             self.mean = 0
+#         self.prop_list = list(map(list, zip(*prop_list)))
+
+#     def update(self, old_length, new_chain, **kwargs):
+#         chain = kwargs.get('full_chain', None)
+#         self.mean, self.cov = update_covariance(old_length, self.cov, self.mean, new_chain)
+#         for proposal in self.prop_list[0]:
+#             proposal.update(cov=self.cov, chain=chain)
+
+#     def __call__(self, x):
+#         proposal = rng.choice(self.prop_list[0], p=self.prop_list[1])
+#         return proposal(x)
 
 
 # class GaussianProposal():
@@ -82,182 +103,200 @@ class ProposalMix():
 #         return x_star, factor
 
 
-class PriorProposal():
+# class PriorProposal():
+#     """
+#     Are you feeling lucky? Uniform prior draws.
+#     """
+#     def __init__(self, prior_min, prior_max, rng=rng):
+#         """
+#         prior_min: vector containing mins of priors
+#         prior_max: vector containing maxes of priors
+#         """
+#         self.rng = rng
+#         self.prior_min = prior_min
+#         self.prior_max = prior_max
+
+#     def update(self, **kwargs):
+#         return None
+
+#     def __call__(self, x):
+#         x_star = rng.uniform(self.prior_min, self.prior_max)
+#         return x_star, 0
+
+
+def am(x, U, S, groups, temp, buffer):
     """
-    Are you feeling lucky? Uniform prior draws.
+    Adaptive Jump Proposal. This function will occasionally
+    use different jump sizes to ensure proper mixing.
+
+    @param x: Parameter vector at current position
+
+    @return: q: New position in parameter space
+    @return: qxy: Forward-Backward jump probability
+
     """
-    def __init__(self, prior_min, prior_max, rng=rng):
-        """
-        prior_min: vector containing mins of priors
-        prior_max: vector containing maxes of priors
-        """
-        self.rng = rng
-        self.prior_min = prior_min
-        self.prior_max = prior_max
 
-    def update(self, **kwargs):
-        return None
+    q = x.copy()
+    qxy = 0
 
-    def __call__(self, x):
-        x_star = rng.uniform(self.prior_min, self.prior_max)
-        return x_star, 0
+    # choose group
+    jumpind = rng.integers(0, len(groups))
+    # jumpind = np.random.randint(0, len(groups))
 
+    # adjust step size
+    prob = rng.random()
+    # prob = np.random.rand()
 
-# AM jump
-class AMProposal():
+    # large jump
+    if prob > 0.97:
+        scale = 10
 
-    def covarianceJumpProposalAM(self, x, iter, beta):
-        """
-        Adaptive Jump Proposal. This function will occasionally
-        use different jump sizes to ensure proper mixing.
+    # small jump
+    elif prob > 0.9:
+        scale = 0.2
 
-        @param x: Parameter vector at current position
-        @param iter: Iteration of sampler
-        @param beta: Inverse temperature of chain
+    # small-medium jump
+    # elif prob > 0.6:
+    #    scale = 0.5
 
-        @return: q: New position in parameter space
-        @return: qxy: Forward-Backward jump probability
+    # standard medium jump
+    else:
+        scale = 1.0
 
-        """
+    # adjust scale based on temperature
+    if temp <= 100:
+        scale *= np.sqrt(temp)
 
-        q = x.copy()
-        qxy = 0
+    # get parmeters in new diagonalized basis
+    y = np.dot(U[jumpind].T, x[groups[jumpind]])
 
-        # choose group
-        jumpind = np.random.randint(0, len(self.groups))
+    # make correlated componentwise adaptive jump
+    ind = np.arange(len(groups[jumpind]))
+    neff = len(ind)
+    cd = 2.4 / np.sqrt(2 * neff) * scale
 
-        # adjust step size
-        prob = np.random.rand()
+    y[ind] = y[ind] + rng.standard_normal(neff) * cd * np.sqrt(S[jumpind][ind])
+    q[groups[jumpind]] = np.dot(U[jumpind], y)
 
-        # large jump
-        if prob > 0.97:
-            scale = 10
-
-        # small jump
-        elif prob > 0.9:
-            scale = 0.2
-
-        # small-medium jump
-        # elif prob > 0.6:
-        #    scale = 0.5
-
-        # standard medium jump
-        else:
-            scale = 1.0
-
-        # adjust scale based on temperature
-        if self.temp <= 100:
-            scale *= np.sqrt(self.temp)
-
-        # get parmeters in new diagonalized basis
-        y = np.dot(self.U[jumpind].T, x[self.groups[jumpind]])
-
-        # make correlated componentwise adaptive jump
-        ind = np.arange(len(self.groups[jumpind]))
-        neff = len(ind)
-        cd = 2.4 / np.sqrt(2 * neff) * scale
-
-        y[ind] = y[ind] + np.random.randn(neff) * cd * np.sqrt(self.S[jumpind][ind])
-        q[self.groups[jumpind]] = np.dot(self.U[jumpind], y)
-
-        return q, qxy
+    return q, qxy
 
 
-class AMProposal():
+def scam(x, U, S, groups, temp, buffer):
     """
-    Adaptive Metropolis
+    Single Component Adaptive Jump Proposal. This function will occasionally
+    jump in more than 1 parameter. It will also occasionally use different
+    jump sizes to ensure proper mixing.
+
+    @param x: Parameter vector at current position
+    @param iter: Iteration of sampler
+    @param beta: Inverse temperature of chain
+
+    @return: q: New position in parameter space
+    @return: qxy: Forward-Backward jump probability
+
     """
-    def __init__(self, ndim, cov=None, alpha=None):
-        self.cov = cov
-        self.alpha = alpha
-        if cov is None:
-            self.cov = np.identity(ndim) * 0.01**2
-        if alpha is None:
-            self.alpha = 2.38**2 / ndim
-        self.cov_chol = np.linalg.cholesky(self.cov)
-        self.ndim = ndim
-        
-    def update(self, **kwargs):
-        cov = kwargs['cov']
-        try:
-            self.cov_chol = np.linalg.cholesky(cov)
-        except LinAlgError:
-            self.cov_chol = np.linalg.cholesky(cov + np.eye(cov.shape[0]) * 5e-15)
 
-    def __call__(self, x):
-        # draw x_star
-        u = rng.standard_normal(len(x))
-        x_star = x + np.sqrt(self.alpha) * self.cov_chol @ u
-        # proposal log ratio factor is 1(symmetric jump)
-        factor = 0
+    q = x.copy()
+    qxy = 0
 
-        return x_star, factor
+    # choose group
+    jumpind = rng.integers(0, len(groups))
+    ndim = len(groups[jumpind])
+
+    # adjust step size
+    # prob = np.random.rand()
+    prob = rng.random()
+
+    # large jump
+    if prob > 0.97:
+        scale = 10
+
+    # small jump
+    elif prob > 0.9:
+        scale = 0.2
+
+    # small-medium jump
+    # elif prob > 0.6:
+
+    # standard medium jump
+    else:
+        scale = 1.0
+
+    # scale = np.random.uniform(0.5, 10)
+
+    # adjust scale based on temperature
+    if temp <= 100:
+        scale *= np.sqrt(temp)
+
+    # get parmeters in new diagonalized basis
+    # y = np.dot(self.U.T, x[self.covinds])
+
+    # make correlated componentwise adaptive jump
+    # ind = np.unique(np.random.randint(0, ndim, 1))
+    ind = np.unique(rng.integers(0, ndim, 1))
+
+    neff = len(ind)
+    cd = 2.4 / np.sqrt(2 * neff) * scale
+
+    # y[ind] = y[ind] + np.random.randn(neff) * cd * np.sqrt(self.S[ind])
+    # q[self.covinds] = np.dot(self.U, y)
+    q[groups[jumpind]] += (
+        rng.standard_normal() * cd * np.sqrt(S[jumpind][ind]) * U[jumpind][:, ind].flatten()
+    )
+
+    return q, qxy
 
 
-class SCAMProposal():
+def de(x, U, S, groups, temp, buffer):
     """
-    Single Component Adaptive Metropolis
+    Differential Evolution Jump. This function will  occasionally
+    use different jump sizes to ensure proper mixing.
+
+    @param x: Parameter vector at current position
+    @param iter: Iteration of sampler
+    @param beta: Inverse temperature of chain
+
+    @return: q: New position in parameter space
+    @return: qxy: Forward-Backward jump probability
+
     """
-    def __init__(self, ndim, cov=None, alpha=None):
-        self.cov = cov
-        self.alpha = alpha
-        if cov is None:
-            self.cov = np.identity(ndim) * 0.01**2
-        if alpha is None:
-            self.alpha = 2.38**2
-        self.cov_evals, self.cov_evec = np.linalg.eig(self.cov)
-        
-    def update(self, **kwargs):
-        cov = kwargs['cov']
-        self.cov_evals, self.cov_evec = np.linalg.eig(cov)
 
-    def __call__(self, x):
-        idx = rng.integers(low=0, high=len(x))
-        uj = rng.standard_normal(len(x)) * self.cov_evals[idx]
-        Dj = self.cov_evec[:, idx]
-        x_star = x + np.sqrt(self.alpha) * Dj @ uj
-        factor = 0
+    # get old parameters
+    q = x.copy()
+    qxy = 0
 
-        return x_star, factor
+    # choose group
+    jumpind = np.random.randint(0, len(groups))
+    ndim = len(groups[jumpind])
 
+    bufsize = len(buffer)
 
-class DEProposal():
-    """
-    Differential Evolution
-    Use after some burn-in period!
-    """
-    def __init__(self, ndim, chain, alpha=None):
-        self.alpha = alpha
-        if alpha is None:
-            self.alpha = 2.38**2 / ndim
-        self.chain = chain
+    # draw a random integer from 0 - iter
+    # mm = np.random.randint(0, bufsize)
+    # nn = np.random.randint(0, bufsize)
+    mm = rng.integers(0, bufsize)
+    nn = rng.integers(0, bufsize)
 
-    def update(self, **kwargs):
-        chain = kwargs['chain']
-        if chain is None:
-            return None
-        self.chain = chain
+    # make sure mm and nn are not the same iteration
+    while mm == nn:
+        nn = rng.integers(0, bufsize)
 
-    def __call__(self, x):
-        temp = rng.choice(self.chain, size=2, replace=True)
-        jump_vec = temp[1, :] - temp[0, :]
+    # get jump scale size
+    prob = rng.random()
 
-        x_star = x + self.alpha * jump_vec
+    # mode jump
+    if prob > 0.5:
+        scale = 1.0
 
-        factor = 0
-        return x_star, factor
+    else:
+        scale = np.random.rand() * 2.4 / np.sqrt(2 * ndim) * np.sqrt(temp)
 
+    for ii in range(ndim):
 
-def pre_proposals(ndim):
-    adapt = AMProposal(ndim)
-    scam = SCAMProposal(ndim)
-    mix = ProposalMix(ndim, [(adapt, 0.5), (scam, 0.5)])
-    return mix
+        # jump size
+        sigma = buffer[mm, groups[jumpind][ii]] - buffer[nn, groups[jumpind][ii]]
 
+        # jump
+        q[groups[jumpind][ii]] += scale * sigma
 
-def stock_proposals(ndim, chain):
-    adapt = AMProposal(ndim)
-    scam = SCAMProposal(ndim)
-    de = DEProposal(ndim, chain)
-    mix = ProposalMix(ndim, [(adapt, 0.25), (scam, 0.15), (de, 0.60)])
-    return mix
+    return q, qxy
