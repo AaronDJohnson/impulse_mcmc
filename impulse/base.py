a1a3a5b479ad3a812cf46f022345e1e6c28d8127
Aaron <Aaron9034@Hotmail.com>
2022-07-22 08:59:39 -0500

    working version
    Next up: resuming

--- impulse/base.py
+++ impulse/base.py
@@ -10,28 +10,6 @@ from impulse.ptsampler import PTSwap
 from impulse.proposals import JumpProposals, am, scam, de
 from impulse.save_data import SaveData
 
-# def sample(lnlike, lnprior, ndim, x0, num_samples=1_000_000, buf_size=50_000,
-#            amweight=30, scamweight=15, deweight=50, loop_iterations=1000,
-#            save=True, outdir='./chains', filename='/chain_1.txt'):
-#     save = SaveData(outdir=outdir, filename=filename)
-#     # set up proposals:
-#     mix = JumpProposals(ndim, buf_size=buf_size)
-#     mix.add_jump(am, amweight)
-#     mix.add_jump(scam, scamweight)
-#     mix.add_jump(de, deweight)
-#     # make empty full chain
-#     full_chain = np.zeros((num_samples, ndim))
-#     # set up count and iterations between loops
-#     sampler = MHSampler(x0, lnlike, lnprior, mix, iterations=loop_iterations)
-#     count = 0
-#     for _ in tqdm(range(0, int(num_samples), loop_iterations)):
-#         chain, like, prob, accept = sampler.sample()
-#         save(chain, like, prob, accept)
-#         full_chain[count:count + loop_iterations, :] = chain
-#         mix.recursive_update(count, chain)
-#         count += loop_iterations
-#     return full_chain
-
 
 class PTSampler():
     def __init__(self, lnlike, lnprior, x0, num_samples=1_000_000, buf_size=50_000, ntemps=8, ncores=1,
@@ -128,8 +106,12 @@ class PTSampler():
 
     def _init_sampler(self):
         """
-        Initialize sampler (one for each temperature)
+        Initialize sampler (one for each temperature) and chains
         """
+        self.chain = np.zeros((self.loop_iterations, self.ndim, self.ntemps))
+        self.lnlike_arr = np.zeros((self.loop_iterations, self.ntemps))
+        self.lnprob_arr = np.zeros((self.loop_iterations, self.ntemps))
+        self.accept_arr = np.zeros((self.loop_iterations, self.ntemps))
         self.samplers = [MHSampler(self.x0[ii], self.lnlike, self.lnprior, self.mixes[ii],
                          iterations=self.swap_count, init_temp=self.ptswap.ladder[ii]) for ii in range(self.ntemps)]
 
@@ -149,7 +131,24 @@ class PTSampler():
         """
         Perform PT step
         """
-        self.ptswap.step()
+        with Pool(nodes=min(self.ntemps, self.ncores)) as p:
+            res = p.map(lambda sampler: sampler.sample(), self.samplers)
+        for ii in range(len(res)):
+            (self.chain[self.swap_tot:self.swap_tot + self.swap_count, :, ii],
+             self.lnlike_arr[self.swap_tot:self.swap_tot + self.swap_count, ii],
+             self.lnprob_arr[self.swap_tot:self.swap_tot + self.swap_count, ii],
+             self.accept_arr[self.swap_tot:self.swap_tot + self.swap_count, ii]) = res[ii]
+        swap_idx = self.samplers[0].num_samples % self.loop_iterations - 1
+        self.chain, self.lnlike_arr, self.logprob_arr = self.ptswap(self.chain, self.lnlike_arr, self.lnprob_arr, swap_idx)
+        if self.adapt:
+            self.ptswap.adapt_ladder(temp_dir=self.temp_dir)
+        [self.samplers[ii].set_x0(self.chain[swap_idx, :, ii], self.logprob_arr[swap_idx, ii], temp=self.ptswap.ladder[ii]) for ii in range(self.ntemps)]
+
+
+    def _save_step(self):
+        for ii in range(self.ntemps):
+            self.saves[ii](self.chain[:, :, ii], self.lnlike_arr[:, ii], self.lnprob_arr[:, ii], self.accept_arr[:, ii])
+            self.mixes[ii].recursive_update(self.sample_count, self.chain[:, :, ii])
 
 
     def add_jump(self, jump, weight):
@@ -157,95 +156,98 @@ class PTSampler():
             self.samplers[ii].add_jump(jump, weight)
 
 
-    # TODO(Aaron): This looks complicated. Clean it up!
     def sample(self):
-        chain = np.zeros((self.loop_iterations, self.ndim, self.ntemps))
-        lnlike_arr = np.zeros((self.loop_iterations, self.ntemps))
-        lnprob_arr = np.zeros((self.loop_iterations, self.ntemps))
-        accept_arr = np.zeros((self.loop_iterations, self.ntemps))
-
         # set up count and iterations between loops
-        count = 0
         for _ in tqdm(range(0, int(self.num_samples), self.loop_iterations)):
-            swap_tot = 0
-            for _ in range(self.loop_iterations // self.swap_count):
-                with Pool(nodes=min(self.ntemps, self.ncores)) as p:
-                    res = p.map(lambda sampler: sampler.sample(), self.samplers)
-                for ii in range(len(res)):
-                    (chain[swap_tot:swap_tot + self.swap_count, :, ii],
-                    lnlike_arr[swap_tot:swap_tot + self.swap_count, ii],
-                    lnprob_arr[swap_tot:swap_tot + self.swap_count, ii],
-                    accept_arr[swap_tot:swap_tot + self.swap_count, ii]) = res[ii]
-                swap_idx = self.samplers[0].num_samples % self.loop_iterations - 1
-                chain, lnlike_arr, logprob_arr = self.ptswap(chain, lnlike_arr, lnprob_arr, swap_idx)
-                if self.adapt:
-                    self.ptswap.adapt_ladder(temp_dir=self.temp_dir)
-                [self.samplers[ii].set_x0(chain[swap_idx, :, ii], logprob_arr[swap_idx, ii], temp=self.ptswap.ladder[ii]) for ii in range(self.ntemps)]
-                # print(samplers[0].x0)
-                swap_tot += self.swap_count
-            for ii in range(self.ntemps):
-                self.saves[ii](chain[:, :, ii], lnlike_arr[:, ii], lnprob_arr[:, ii], accept_arr[:, ii])
-                self.mixes[ii].recursive_update(count, chain[:, :, ii])
+            self.swap_tot = 0  # count the samples between swaps
+            for _ in range(self.loop_iterations // self.swap_count):  # swap loops
+                self._ptstep()
+                self.swap_tot += self.swap_count
+            self._save_step()
             if self.ret_chain:
-                self.full_chain[count:count + self.loop_iterations, :, :] = chain
-            count += self.loop_iterations
+                self.full_chain[self.sample_count:self.sample_count + self.loop_iterations, :, :] = self.chain
+            self.sample_count += self.loop_iterations
         if self.ret_chain:
             return self.full_chain
 
 
-# TODO: make this a class and combine these two functions with cleaner counting built in
-def pt_sample(lnlike, lnprior, ndim, x0, num_samples=1_000_000, buf_size=50_000,
-              amweight=30, scamweight=15, deweight=50, ntemps=2, ncores=1, tmin=1, tmax=None, tstep=None,
-              swap_count=100, ladder=None, tinf=False, adapt=True, adapt_t0=100, adapt_nu=10,
-              loop_iterations=1000, outdir='./chains', temp_dir=None, ret_chain=False):
+# # TODO: make this a class and combine these two functions with cleaner counting built in
+# def pt_sample(lnlike, lnprior, ndim, x0, num_samples=1_000_000, buf_size=50_000,
+#               amweight=30, scamweight=15, deweight=50, ntemps=2, ncores=1, tmin=1, tmax=None, tstep=None,
+#               swap_count=100, ladder=None, tinf=False, adapt=True, adapt_t0=100, adapt_nu=10,
+#               loop_iterations=1000, outdir='./chains', temp_dir=None, ret_chain=False):
 
-    ptswap = PTSwap(ndim, ntemps, tmin=tmin, tmax=tmax, tstep=tstep,
-                    tinf=tinf, adapt_t0=adapt_t0,
-                    adapt_nu=adapt_nu, ladder=ladder)
-    saves = [SaveData(outdir=outdir, filename='/chain_{}.txt'.format(ptswap.ladder[ii])) for ii in range(ntemps)]
+#     ptswap = PTSwap(ndim, ntemps, tmin=tmin, tmax=tmax, tstep=tstep,
+#                     tinf=tinf, adapt_t0=adapt_t0,
+#                     adapt_nu=adapt_nu, ladder=ladder)
+#     saves = [SaveData(outdir=outdir, filename='/chain_{}.txt'.format(ptswap.ladder[ii])) for ii in range(ntemps)]
 
-    # make empty full chain
-    if ret_chain:
-        full_chain = np.zeros((num_samples, ndim, ntemps))
-    chain = np.zeros((loop_iterations, ndim, ntemps))
-    lnlike_arr = np.zeros((loop_iterations, ntemps))
-    lnprob_arr = np.zeros((loop_iterations, ntemps))
-    accept_arr = np.zeros((loop_iterations, ntemps))
-    # set up proposals
-    mixes = []
-    for ii in range(ntemps):
-        mixes.append(JumpProposals(ndim, buf_size=buf_size))
-        mixes[ii].add_jump(am, amweight)
-        mixes[ii].add_jump(scam, scamweight)
-        mixes[ii].add_jump(de, deweight)
+#     # make empty full chain
+#     if ret_chain:
+#         full_chain = np.zeros((num_samples, ndim, ntemps))
+#     chain = np.zeros((loop_iterations, ndim, ntemps))
+#     lnlike_arr = np.zeros((loop_iterations, ntemps))
+#     lnprob_arr = np.zeros((loop_iterations, ntemps))
+#     accept_arr = np.zeros((loop_iterations, ntemps))
+#     # set up proposals
+#     mixes = []
+#     for ii in range(ntemps):
+#         mixes.append(JumpProposals(ndim, buf_size=buf_size))
+#         mixes[ii].add_jump(am, amweight)
+#         mixes[ii].add_jump(scam, scamweight)
+#         mixes[ii].add_jump(de, deweight)
 
-    # make set of samplers (1 for each temp)
-    samplers = [MHSampler(x0[ii], lnlike, lnprior, mixes[ii], iterations=swap_count, init_temp=ptswap.ladder[ii]) for ii in range(ntemps)]
+#     # make set of samplers (1 for each temp)
+#     samplers = [MHSampler(x0[ii], lnlike, lnprior, mixes[ii], iterations=swap_count, init_temp=ptswap.ladder[ii]) for ii in range(ntemps)]
 
-    # set up count and iterations between loops
-    count = 0
-    for _ in tqdm(range(0, int(num_samples), loop_iterations)):
-        swap_tot = 0
-        for _ in range(loop_iterations // swap_count):
-            with Pool(nodes=min(ntemps, ncores)) as p:
-                res = p.map(lambda sampler: sampler.sample(), samplers)
-            for ii in range(len(res)):
-                (chain[swap_tot:swap_tot + swap_count, :, ii],
-                 lnlike_arr[swap_tot:swap_tot + swap_count, ii],
-                 lnprob_arr[swap_tot:swap_tot + swap_count, ii],
-                 accept_arr[swap_tot:swap_tot + swap_count, ii]) = res[ii]
-            swap_idx = samplers[0].num_samples % loop_iterations - 1
-            chain, lnlike_arr, logprob_arr = ptswap(chain, lnlike_arr, lnprob_arr, swap_idx)
-            if adapt:
-                ptswap.adapt_ladder(temp_dir=temp_dir)
-            [samplers[ii].set_x0(chain[swap_idx, :, ii], logprob_arr[swap_idx, ii], temp=ptswap.ladder[ii]) for ii in range(ntemps)]
-            # print(samplers[0].x0)
-            swap_tot += swap_count
-        for ii in range(ntemps):
-            saves[ii](chain[:, :, ii], lnlike_arr[:, ii], lnprob_arr[:, ii], accept_arr[:, ii])
-            mixes[ii].recursive_update(count, chain[:, :, ii])
-        if ret_chain:
-            full_chain[count:count + loop_iterations, :, :] = chain
-        count += loop_iterations
-    if ret_chain:
-        return full_chain
+#     # set up count and iterations between loops
+#     count = 0
+#     for _ in tqdm(range(0, int(num_samples), loop_iterations)):
+#         swap_tot = 0
+#         for _ in range(loop_iterations // swap_count):
+#             with Pool(nodes=min(ntemps, ncores)) as p:
+#                 res = p.map(lambda sampler: sampler.sample(), samplers)
+#             for ii in range(len(res)):
+#                 (chain[swap_tot:swap_tot + swap_count, :, ii],
+#                  lnlike_arr[swap_tot:swap_tot + swap_count, ii],
+#                  lnprob_arr[swap_tot:swap_tot + swap_count, ii],
+#                  accept_arr[swap_tot:swap_tot + swap_count, ii]) = res[ii]
+#             swap_idx = samplers[0].num_samples % loop_iterations - 1
+#             chain, lnlike_arr, logprob_arr = ptswap(chain, lnlike_arr, lnprob_arr, swap_idx)
+#             if adapt:
+#                 ptswap.adapt_ladder(temp_dir=temp_dir)
+#             [samplers[ii].set_x0(chain[swap_idx, :, ii], logprob_arr[swap_idx, ii], temp=ptswap.ladder[ii]) for ii in range(ntemps)]
+#             # print(samplers[0].x0)
+#             swap_tot += swap_count
+#         for ii in range(ntemps):
+#             saves[ii](chain[:, :, ii], lnlike_arr[:, ii], lnprob_arr[:, ii], accept_arr[:, ii])
+#             mixes[ii].recursive_update(count, chain[:, :, ii])
+#         if ret_chain:
+#             full_chain[count:count + loop_iterations, :, :] = chain
+#         count += loop_iterations
+#     if ret_chain:
+#         return full_chain
+
+
+# def sample(lnlike, lnprior, ndim, x0, num_samples=1_000_000, buf_size=50_000,
+#            amweight=30, scamweight=15, deweight=50, loop_iterations=1000,
+#            save=True, outdir='./chains', filename='/chain_1.txt'):
+#     save = SaveData(outdir=outdir, filename=filename)
+#     # set up proposals:
+#     mix = JumpProposals(ndim, buf_size=buf_size)
+#     mix.add_jump(am, amweight)
+#     mix.add_jump(scam, scamweight)
+#     mix.add_jump(de, deweight)
+#     # make empty full chain
+#     full_chain = np.zeros((num_samples, ndim))
+#     # set up count and iterations between loops
+#     sampler = MHSampler(x0, lnlike, lnprior, mix, iterations=loop_iterations)
+#     count = 0
+#     for _ in tqdm(range(0, int(num_samples), loop_iterations)):
+#         chain, like, prob, accept = sampler.sample()
+#         save(chain, like, prob, accept)
+#         full_chain[count:count + loop_iterations, :] = chain
+#         mix.recursive_update(count, chain)
+#         count += loop_iterations
+#     return full_chain
+
