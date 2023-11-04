890315ae6a9c8e050bd58d8423c2c1589214afe7
Aaron <Aaron9034@Hotmail.com>
2023-11-04 11:49:59 -0700

    add SeedSequence

--- impulse/base.py
+++ impulse/base.py
@@ -20,9 +20,10 @@ class ShortChain:
     short_iters: int
     iteration: int = 1
     outdir: str = './chains/'
-    filename: str = 'chain_1.txt'
+    filename: str = 'chain_.txt'
     resume: bool = False
     thin: int = 1
+    temp: float = 1.0
 
     def __post_init__(self):
         if self.thin > self.short_iters:
@@ -31,8 +32,8 @@ class ShortChain:
         self.lnprob = np.zeros((self.short_iters))
         self.lnlike = np.zeros((self.short_iters))
         self.accept = np.zeros((self.short_iters))
-
         self.filepath = os.path.join(self.outdir, self.filename)
+        self.filepath = os.path.join(self.filepath, str(self.temp))
 
         pathlib.Path(self.outdir).mkdir(parents=True, exist_ok=True)
         if self.exists(self.outdir, self.filename) and not self.resume:
@@ -55,7 +56,74 @@ class ShortChain:
         with open(self.filepath, 'a+') as f:
             np.savetxt(f, to_save)
 
-class TestSampler:
+# class TestSampler:
+#     def __init__(self,
+#                  ndim: int,
+#                  lnlike: Callable,
+#                  lnprior: Callable,
+#                  buffer_size: int = 50_000,
+#                  sample_mean: float = None,
+#                  sample_cov: np.ndarray = None,
+#                  groups: list = None,
+#                  loglargs: list = [],
+#                  loglkwargs: dict = {},
+#                  logpargs: list = [],
+#                  logpkwargs: dict = {},
+#                  cov_update: int = 100,
+#                  save_freq: int = 1000,
+#                  scam_weight: float = 30,
+#                  am_weight: float = 15,
+#                  de_weight: float = 50,
+#                  seed: int = None,
+#                  outdir: str = './chains'
+#                  ) -> None:
+
+#         self.ndim = ndim
+#         self.lnlike = _function_wrapper(lnlike, loglargs, loglkwargs)
+#         self.lnprior = _function_wrapper(lnprior, logpargs, logpkwargs)
+#         self.rng = np.random.default_rng(seed)  # change this to seedsequence later on!
+
+#         self.chain_stats = ChainStats(ndim, self.rng, groups=groups, sample_cov=sample_cov,
+#                                     sample_mean=sample_mean, buffer_size=buffer_size)
+#         self.jumps = JumpProposals(self.chain_stats)
+#         self.jumps.add_jump(am, am_weight)
+#         self.jumps.add_jump(scam, scam_weight)
+#         self.jumps.add_jump(de, de_weight)
+
+#         self.cov_update = cov_update
+#         self.save_freq = save_freq
+#         self.outdir = outdir
+
+#     def sample(self,
+#                initial_sample: np.ndarray,
+#                num_iterations: int,
+#                thin: int = 1):
+
+#         short_chain = ShortChain(self.ndim, self.save_freq, thin=thin)  # keep save_freq samples
+
+#         # set up initial state here:
+#         x0 = np.array(initial_sample, dtype=np.float64)
+#         lnlike0 = self.lnlike(x0)
+#         lnprior0 = self.lnprior(x0)
+#         initial_state = MHState(x0,
+#                                 lnlike0,
+#                                 lnprior0,
+#                                 lnlike0 + lnprior0
+#                                 )
+
+#         # initial sample and go!
+#         state = mh_kernel(initial_state, self.jumps, self.lnlike,
+#                           self.lnprior, self.rng)
+#         short_chain.add_state(state)
+#         for jj in tqdm(range(1, num_iterations), initial=1, total=num_iterations):
+#             state = mh_kernel(state, self.jumps, self.lnlike, self.lnprior, self.rng)
+#             short_chain.add_state(state)
+#             if jj % self.cov_update == 0:
+#                 self.chain_stats.recursive_update(self.chain_stats.sample_total, short_chain.samples)
+#             if jj % self.save_freq == 0:
+#                 short_chain.save_chain()
+
+class PTTestSampler:
     def __init__(self,
                  ndim: int,
                  lnlike: Callable,
@@ -74,16 +142,19 @@ class TestSampler:
                  am_weight: float = 15,
                  de_weight: float = 50,
                  seed: int = None,
-                 outdir: str = './chains'
+                 outdir: str = './chains',
+                 num_temps: int = 2
                  ) -> None:
 
         self.ndim = ndim
         self.lnlike = _function_wrapper(lnlike, loglargs, loglkwargs)
         self.lnprior = _function_wrapper(lnprior, logpargs, logpkwargs)
-        self.rng = np.random.default_rng(seed)  # change this to seedsequence later on!
+        sequence = np.random.SeedSequence(seed)
+        sequence.spawn(num_temps)
+        self.rngs = [np.random.default_rng(s) for s in sequence]
 
         self.chain_stats = ChainStats(ndim, self.rng, groups=groups, sample_cov=sample_cov,
-                                    sample_mean=sample_mean, buffer_size=buffer_size)
+                                      sample_mean=sample_mean, buffer_size=buffer_size)
         self.jumps = JumpProposals(self.chain_stats)
         self.jumps.add_jump(am, am_weight)
         self.jumps.add_jump(scam, scam_weight)
@@ -98,6 +169,7 @@ class TestSampler:
                num_iterations: int,
                thin: int = 1):
 
+        # set up temperatures
         short_chain = ShortChain(self.ndim, self.save_freq, thin=thin)  # keep save_freq samples
 
         # set up initial state here:
@@ -115,7 +187,8 @@ class TestSampler:
                           self.lnprior, self.rng)
         short_chain.add_state(state)
         for jj in tqdm(range(1, num_iterations), initial=1, total=num_iterations):
-            state = mh_kernel(state, self.jumps, self.lnlike, self.lnprior, self.rng)
+            for ii in range(1, ntemps):
+                state = mh_kernel(state, self.jumps, self.lnlike, self.lnprior, self.rng)
             short_chain.add_state(state)
             if jj % self.cov_update == 0:
                 self.chain_stats.recursive_update(self.chain_stats.sample_total, short_chain.samples)
--- impulse/ptsampler.py
+++ impulse/ptsampler.py
@@ -1,13 +1,70 @@
 import numpy as np
 from dataclasses import dataclass
-from typing import Callable
 import ray
-
 
 @dataclass
 class PTState():
-    pass
+    """
+    Parallel Tempering State
+    Contains the PT ladder, the swap acceptance rate, and the number of swaps
+    """
+    ndim: int
+    ntemps: int
+    swap_steps: int = 100
+    min_temp: float = 1.0
+    max_temp: float = None
+    temp_step: float = None
+    swap_accept: float = 0.0
+    nswaps: int = 1  # start at 1 to avoid divide by zero errors
+    ladder: np.ndarray = None
+    inf_temp: bool = False
+    # adaptive temperature ladder parameters:
+    adapt_t0: float = 100
+    adapt_nu: float = 10
 
+    def __post_init__(self):
+        if self.ladder is None:
+            self.ladder = self.temp_ladder()
+
+    def compute_accept_ratio(self):
+        return self.swap_accept / self.nswaps
+
+    def temp_ladder(self):
+        """
+        Method to compute temperature ladder. At the moment this uses
+        a geometrically spaced temperature ladder with a temperature
+        spacing designed to give 25% temperature swap acceptance rate
+        on a multi-variate Gaussian.
+        """
+        if self.inf_temp:
+            self.ntemps -= 1  # remove top value from ladder
+        if self.temp_step is None and self.max_temp is None:
+            self.temp_step = 1 + np.sqrt(2 / self.ndim)
+        elif self.temp_step is None and self.max_temp is not None:
+            self.temp_step = np.exp(np.log(self.max_temp / self.min_temp) / (self.ntemps - 1))
+        temp_idxs = np.arange(self.ntemps)
+        if self.inf_temp:
+            self.ntemps += 1 # add empty top value back to ladder
+            ladder = self.min_temp * self.temp_step**temp_idxs  # compute ladder
+            ladder = np.concatenate([ladder, [np.inf]])  # add inf value as top of ladder
+        else:
+            ladder = self.min_temp * self.temp_step**temp_idxs
+        return ladder
+
+    def adapt_ladder(self):
+        """
+        Adapt temperatures according to arXiv:1501.05823 <https://arxiv.org/abs/1501.05823>.
+        """
+        # Temperature adjustments with a hyperbolic decay.
+        decay = self.adapt_t0 / (self.nswaps + self.adapt_t0)  # t0 / (t + t0)
+        kappa = decay / self.adapt_nu  # 1 / nu
+        # Construct temperature adjustments.
+        accept_ratio = self.compute_accept_ratio()
+        dscaled_accept = kappa * (accept_ratio[:-1] - accept_ratio[1:])  # delta acceptance ratios for chains
+        # Compute new ladder (hottest and coldest chains don't move).
+        delta_temps = np.diff(self.ladder[:-1])
+        delta_temps *= np.exp(dscaled_accept)
+        self.ladder[1:-1] = (np.cumsum(delta_temps) + self.ladder[0])
 
 @ray.remote
 class Collection():
@@ -56,41 +113,11 @@ class PTSwap():
 
         self.collection = Collection.remote()
 
-    def temp_ladder(self):
-        """
-        Method to compute temperature ladder. At the moment this uses
-        a geometrically spaced temperature ladder with a temperature
-        spacing designed to give 25 % temperature swap acceptance rate
-        in a multi-variate Gaussian.
-        """
-        if self.tstep is None and self.tmax is None:
-            self.tstep = 1 + np.sqrt(2 / self.ndim)
-        elif self.tstep is None and self.tmax is not None:
-            self.tstep = np.exp(np.log(self.tmax / self.tmin) / (self.ntemps - 1))
-        ii = np.arange(self.ntemps)
-        ladder = self.tmin * self.tstep**ii
-        return ladder
-
     def get_temp_ladder(self):
         return self.ladder
 
     def get_swap_steps(self):
         return self.swap_steps
-
-    def adapt_ladder(self):
-        """
-        Adapt temperatures according to arXiv:1501.05823 <https://arxiv.org/abs/1501.05823>.
-        """
-        # Modulate temperature adjustments with a hyperbolic decay.
-        decay = self.adapt_t0 / (self.nswaps + self.adapt_t0)  # t0 / (t + t0)
-        kappa = decay / self.adapt_nu  # 1 / nu
-        # Construct temperature adjustments.
-        accept_ratio = self.compute_accept_ratio()
-        dSs = kappa * (accept_ratio[:-1] - accept_ratio[1:])  # delta acceptance ratios for chains
-        # Compute new ladder (hottest and coldest chains don't move).
-        deltaTs = np.diff(self.ladder[:-1])
-        deltaTs *= np.exp(dSs)
-        self.ladder[1:-1] = (np.cumsum(deltaTs) + self.ladder[0])
 
     def compute_accept_ratio(self):
         return self.swap_accept / self.nswaps
