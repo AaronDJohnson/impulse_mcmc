278b49a32471bd2ab6784c280e46792dd2c19c46
Aaron <Aaron9034@Hotmail.com>
2025-01-23 13:53:47 -0800

    add swap between two sources

--- impulse/base.py
+++ impulse/base.py
@@ -167,6 +167,29 @@ class PTTestSampler:
 
     def add_custom_jump(self, proposal, weight):
         [jump.add_jump(proposal, weight) for jump in self.jumps]
+    
+    def source_swap_proposal(self, chain_stats):
+        rng = chain_stats.rng
+        q = chain_stats.current_sample.copy()
+        qxy = 0
+        nmodel = int(np.rint(q[-1]))
+        if nmodel == 0:
+            return q, qxy
+        swap_source_1 = rng.integers(0, nmodel + 1)
+        swap_source_2 = rng.integers(0, nmodel + 1)
+        if swap_source_1 == swap_source_2:
+            return q, qxy
+        # print(nmodel)
+        # print(swap_source_1, swap_source_2)
+        # print(q[self.num_params * swap_source_1:self.num_params * (swap_source_1 + 1)])
+        # print(q[self.num_params * swap_source_2:self.num_params * (swap_source_2 + 1)])
+        # print()
+        x = q[3 * swap_source_1:3 * (swap_source_1 + 1)].copy()
+        y = q[3 * swap_source_2:3 * (swap_source_2 + 1)].copy()
+
+        q[3 * swap_source_1:3 * (swap_source_1 + 1)] = y
+        q[3 * swap_source_2:3 * (swap_source_2 + 1)] = x
+        return q, qxy
 
 class _function_wrapper(object):
     """
